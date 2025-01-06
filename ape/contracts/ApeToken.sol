22e1ae207643eea08d51e44ddbf6c46d01d9eeac
Guil. Sperb Machado <guil@axlabs.com>
2025-01-06 01:06:30 +0100

    ape support

--- ape/contracts/ApeToken.sol
+++ ape/contracts/ApeToken.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
+contract ApeToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("ApeToken", "STK") {
         _mint(msg.sender, initialSupply);
     }
 }
